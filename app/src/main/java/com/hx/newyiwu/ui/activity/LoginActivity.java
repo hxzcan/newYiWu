8100b17be633ba22422b15d705782eb17cccef00
hxzcan <362970502@qq.com>
2017-08-31 14:29:52 +0800

    修改了Edittext的样式，以及点击EditText以外的地方软件盘消失

--- app/src/main/java/com/hx/newyiwu/ui/activity/LoginActivity.java
+++ app/src/main/java/com/hx/newyiwu/ui/activity/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.hx.newyiwu.ui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -45,13 +52,12 @@ public class LoginActivity extends AppCompatActivity  implements CommonContract.
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.login,R.id.user_register})
+    @OnClick({R.id.login,R.id.forget_password,R.id.user_register,R.id.login_service,R.id.tel})
     public void onClick(View view){
         switch (view.getId()){
-            case R.id.login:
+            case R.id.login://登录按钮
                 String tel=mTel.getText().toString().trim();
                 String password=mPassword.getText().toString().trim();
-
                 if (!TextUtils.isEmpty(tel)&&!TextUtils.isEmpty(password)){
                     mLoginPresenter=new LoginPresenter(this,tel,password);
                     mLoginPresenter.getData();
@@ -59,9 +65,37 @@ public class LoginActivity extends AppCompatActivity  implements CommonContract.
                     Toast.makeText(this, "密码或者用户名不能为空", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.user_register:
+
+            case R.id.forget_password://忘记密码
+
+                break;
+
+            case R.id.user_register://注册
                 Intent intent=new Intent(this,RegisterActivity.class);
                 startActivity(intent);
+                break;
+
+            case R.id.login_service://服务
+
+                break;
+
+            case R.id.tel://当删除账号的时候，密码置为空
+                mTel.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        mPassword.setText("");//删除账号，密码设置为空
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
                 break;
             default:
                 break;
@@ -102,5 +136,45 @@ public class LoginActivity extends AppCompatActivity  implements CommonContract.
     @Override
     public void showBottomText() {
 
+    }
+
+    //键盘收回
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction()==MotionEvent.ACTION_DOWN){
+            View v=getCurrentFocus();
+            if (isShouldHideInput(v,ev)){
+                InputMethodManager inputMethodManager= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (inputMethodManager!=null){
+                    //键盘消失
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(),0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        //必不可少，不然所有的组件都会失去TouchEvent
+        if (getWindow().superDispatchTouchEvent(ev)){
+            return  true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    private boolean isShouldHideInput(View view,MotionEvent motionEvent){
+       if (view!=null&&(view instanceof EditText)){
+           int[] leftTop={0,0};
+            //获取输出框当前的位置
+           view.getLocationInWindow(leftTop);
+           int left=leftTop[0];
+           int top=leftTop[1];
+           int bottom=top+view.getHeight();
+           int right=left+view.getWidth();
+           if (motionEvent.getX()>left&&motionEvent.getX()<right&&motionEvent.getY()>top&&motionEvent.getY()<bottom){
+               //点击是editText范围内保留editText状态
+               return  false;
+           }else{
+               return  true;
+           }
+       }
+        return false;
     }
 }
